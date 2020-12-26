ba00a33f201d547281a9dabddbc38409a2e8d941
Michael George <mdgeorge@cs.cornell.edu>
2020-12-26 09:24:37 -0500

    fixed routes so everything routes to prototype

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,25 +1,8 @@
 
 const routes = [
   {
-    path: '/prototype',
-    component: () => import('pages/Prototype.vue'),
-  },
-
-  {
-    path: '/host',
-    component: () => import('pages/Host.vue'),
-  },
-
-  {
-    path: '/client',
-    component: () => import('pages/Client.vue'),
-  },
-
-  // Always leave this as last one,
-  // but you can also remove it
-  {
     path: '*',
-    component: () => import('pages/Error404.vue'),
+    component: () => import('pages/Prototype.vue'),
   },
 ]
 
