1d4ceecfb20a70d77f4c17763517c9de3011cd47
Michael George <mdgeorge@cs.cornell.edu>
2020-12-30 14:38:19 -0500

    further simplification/refinement of rtc store

--- src/store/rtc.js
+++ src/store/rtc.js
@@ -11,14 +11,16 @@ export default {
     nextConn:      null,
     answerHandler: null,
 
-    streams:   {},
+    localStream: null,
+    remoteStreams: {},
 
     rtcConfig: {},
     vidConfig: { audio: false, video: true, aspectRatio: 1.7777 },
   },
 
   getters: {
-    stream: (state) => (id) => state.streams[id],
+    stream: (state,g,rootState) => (id) =>
+      id == rootState.id ? state.localStream : state.remoteStreams[id],
     index: (s,g,rootState) => rootState.users.findIndex((user) => user.id == rootState.id),
   },
 
@@ -28,10 +30,12 @@ export default {
         Vue.set(state.streams, users[i].id, null);
     },
 
-    SET_TRACK(state, track) {
-      const stream = new MediaStream();
-      stream.addTrack(track);
-      Vue.set(state.streams, track.contentHint, stream);
+    SET_MYSTREAM(state, stream) {
+      state.localStream = stream;
+    },
+
+    SET_REMOTE_STREAMS(state, streams) {
+      state.remoteStreams = streams;
     },
 
     SET_PREV(state, {prevID, prevConn, offerHandler}) {
@@ -57,12 +61,8 @@ export default {
       };
 
       let stream = await navigator.mediaDevices.getUserMedia(constraints);
-      let id     = await context.rootState.id_promise;
 
-      console.log(stream.getTracks());
-      let track  = stream.getTracks()[0];
-      track.contentHint = id;
-      context.commit('SET_TRACK', track);
+      context.commit('SET_MYSTREAM', stream);
     },
 
     socket_update(context, users) {
@@ -71,140 +71,12 @@ export default {
     },
 
     async updatePrev(context) {
-      let n = context.getters.index;
-      const prevID = n <= 0 ? null : context.rootState.users[n - 1].id;
-
-      if (prevID == context.state.prevID)
-        // no change
-        return;
-
-      // fast-forwarding: nullify all past tracks
-      context.commit('CLEAR_TRACKS');
-
-      // TODO: tear down old connection
-
-      if (prevID == null)
-        // we're the soloist; no incoming streams
-        return;
-
-      // set up new incoming RTC stream
-      const peer  = new RTCPeerConnection(context.state.rtcConfig);
-      // receive tracks from prev
-      peer.addEventListener('track', function(event) {
-        console.log('received track', track);
-        context.commit('SET_TRACK', track);
-      });
-
-      const offer = await new Promise(function (offerHandler,reject) {
-        context.commit('SET_PREV', {prevID, prevConn:peer, offerHandler});
-      });
-
-      console.log("offer received");
-
-      peer.setRemoteDescription(offer);
-      const answer = await peer.createAnswer();
-      await peer.setLocalDescription(answer);
-      this._vm.$socket.client.emit('direct message', {recipient: prevID, answer});
-
-      // forward ICE candidates to prev
-      const that = this;
-      peer.addEventListener('icecandidate', function(event) {
-        // console.log("ice event: ", event);
-        if (event.candidate)
-          that._vm.$socket.client.emit('direct message', {recipient: prevID, candidate: event.candidate});
-      });
-
-      peer.addEventListener('connectionstatechange', function (event) {
-        if (peer.connectionState == 'connected')
-          console.log('connected!');
-      });
     },
 
     async updateNext(context) {
-      let n = context.getters.index;
-      const nextID = n == context.rootState.users.length - 1 || n < 0
-                   ? null : context.rootState.users[n + 1].id;
-
-      if (nextID == context.state.nextID)
-        // no change
-        return;
-
-      // TODO: tear down old connection
-
-      // set up new RTC offer
-      if (nextID == null)
-        return;
-
-      // set up new outgoing RTC stream
-      const peer  = new RTCPeerConnection(context.state.rtcConfig);
-      const offer = await peer.createOffer({offerToReceiveVideo: 1});
-
-      await peer.setLocalDescription(offer);
-      const that = this;
-      const answer = await new Promise(function (answerHandler, reject) {
-        context.commit('SET_NEXT', {nextID, nextConn: peer, answerHandler});
-        that._vm.$socket.client.emit('direct message', {recipient: nextID, offer});
-      });
-
-      console.log("answer received");
-
-      await peer.setRemoteDescription(answer);
-
-      // forward ICE candidates to prev
-      peer.addEventListener('icecandidate', function (event) {
-        // console.log("ice event: ", event);
-        if (event.candidate)
-          this._vm.$socket.client.emit('direct message', {recipient: nextID, candidate: event.candidate});
-      });
-
-      // wait for connection
-      await new Promise(function (resolve, reject) {
-        peer.addEventListener('connectionstatechange', function (event) {
-          if (peer.connectionState == 'connected')
-            resolve();
-            console.log("connected!");
-        });
-      });
-      
-      // forward tracks to remote peer
-      for (let i = 0; i <= context.getters.index; i++) {
-        let id     = context.rootState.users[i].id;
-        let stream = new MediaStream();
-        let unsubscribe = this._vm.$watch(
-          () => context.state.streams[id], // watch expression
-          function() {                    // callback
-            let track = context.state.streams[id].getTracks()[0];
-            console.log("track watch callback", id, track);
-            if (track) {
-              console.log("adding track", track, stream);
-              peer.addTrack(track, stream);
-            }
-          },
-          { immediate: true }
-        );
-      }
     },
 
     socket_directMessage(context, {senderID, ...msg}) {
-      console.log("received direct message");
-      console.log("   prevID: ", context.state.prevID);
-      console.log("   nextID: ", context.state.nextID);
-      console.log("   msg:    ", msg);
-
-      if(senderID == context.state.prevID       && msg.offer)
-        context.state.offerHandler(msg.offer);
-
-      else if (senderID == context.state.prevID && msg.candidate)
-        context.state.prevConn.addIceCandidate(msg.candidate);
-
-      else if (senderID == context.state.nextID && msg.answer)
-        context.state.answerHandler(msg.answer);
-
-      else if (senderID == context.state.nextID && msg.candidate)
-        context.state.nextConn.addIceCandidate(msg.candidate);
-
-      else
-        console.log('warning: unexpected direct message');
     },
 
   },
