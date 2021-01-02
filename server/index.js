4c8217eea8c33026fdb106ef2db5dd599ba46ccb
Michael George <mdgeorge@cs.cornell.edu>
2021-01-02 11:51:03 -0500

    consolidated logging, created update function

--- server/index.js
+++ server/index.js
@@ -4,40 +4,47 @@ const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
-let users = [];
+let users    = [];
+let sequence = 0;
+
+function update(newUsers) {
+  users = newUsers;
+  sequence++;
+  let message = {users, sequence};
+  console.log("sending update message: ", message);
+  io.emit('update', message);
+}
 
 io.sockets.on('connection', function (socket) {
   let user = {id: socket.id};
-  users.push(user);
+  update(users.concat(user));
 
-  socket.on('update', function(update) {
-    Object.assign(user, update);
-    console.log('update', users);
-    io.emit('update', users);
+  socket.on('update', function(data) {
+    console.log('processing update');
+    Object.assign(user, data);
+    update(users);
   });
 
   socket.on('cycle', function() {
-    console.log('cycle');
+    console.log('processing cycle');
     let nextSolo = users.findIndex((user,i) => i > 0 && user.playing == "solo");
     nextSolo = nextSolo == -1 ? 1 : nextSolo;
-    users = users.slice(nextSolo).concat(users.slice(0,nextSolo));
-    io.emit('update', users);
+    update(users.slice(nextSolo).concat(users.slice(0,nextSolo)));
   });
 
   socket.on('fast forward', function() {
-    console.log('ff');
+    console.log('processing ff');
   });
 
   socket.on('disconnect', function() {
-    console.log('disconnect');
+    console.log('processing disconnect');
     let n = users.indexOf(user);
-    users = users.slice(0,n).concat(users.slice(n+1));
-    io.emit('update', users);
+    update(users.slice(0,n).concat(users.slice(n+1)));
   });
 
   socket.on('direct message', function({recipient, ...message}) {
+    console.log('processing direct message');
     let msg = {senderID: user.id, ...message};
-    console.log('direct message', msg);
     socket.to(recipient).emit('direct message', msg);
   });
 
